1e6e313db9a469a63d57a879fd608b5a8032b02d
unknown <rbutler@udel.edu>
2018-12-03 19:09:45 -0500

    front end stuff, some (maybe incorrect) routing

--- dogapp/src/app/app.module.ts
+++ dogapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DogsurveyComponent } from './dogsurvey/dogsurvey.component';
@@ -9,6 +9,12 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { WhatwedoComponent } from './whatwedo/whatwedo.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '',component: HomePageComponent},
+  {path: 'survey', component: DogsurveyComponent}
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +28,12 @@ import { NavigationbarComponent } from './navigationbar/navigationbar.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
