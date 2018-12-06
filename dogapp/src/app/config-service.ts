702c4ae2a0d3296f0041e5b456142ec034d1dbb7
Jurban36 <Jennymurban@gmail.com>
2018-12-05 21:25:55 -0500

    pooling

--- dogapp/src/app/config-service.ts
+++ dogapp/src/app/config-service.ts
@@ -20,7 +20,7 @@ export class ConfigService {
     token = 'hey there its me ya boi'
     
     getAllDogs = ():Observable<any> => {
-        this.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzA4NWYxNDMzYjJhMmExMzA5MWYzM2YiLCJmaXJzdE5hbWUiOiJKZW5ueSIsImxhc3ROYW1lIjoiVXJiYW4iLCJlbWFpbCI6Imdhcm9jb2xhMjJAeWFob28uY29tIiwicGhvbmUiOiI5MTczOTk4OTA3IiwiaWF0IjoxNTQ0MDUyNTAwLCJleHAiOjE1NDQwNjI1ODB9.cXuNTsczoiUvgq0_OLyfKlVCHYnBmIWbfcBlygZPTu8'
+        // this.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzA4NWYxNDMzYjJhMmExMzA5MWYzM2YiLCJmaXJzdE5hbWUiOiJKZW5ueSIsImxhc3ROYW1lIjoiVXJiYW4iLCJlbWFpbCI6Imdhcm9jb2xhMjJAeWFob28uY29tIiwicGhvbmUiOiI5MTczOTk4OTA3IiwiaWF0IjoxNTQ0MDUyNTAwLCJleHAiOjE1NDQwNjI1ODB9.cXuNTsczoiUvgq0_OLyfKlVCHYnBmIWbfcBlygZPTu8'
         const configUrl = "http://localhost:3000/api/dogs/allDogs";
         const h: Headers=new Headers();
         return this.http.get(configUrl,new RequestOptions({
@@ -30,7 +30,8 @@ export class ConfigService {
           }));
     }
     getAuth = ():Observable<any> =>{
-        this.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzA4NWYxNDMzYjJhMmExMzA5MWYzM2YiLCJmaXJzdE5hbWUiOiJKZW5ueSIsImxhc3ROYW1lIjoiVXJiYW4iLCJlbWFpbCI6Imdhcm9jb2xhMjJAeWFob28uY29tIiwicGhvbmUiOiI5MTczOTk4OTA3IiwiaWF0IjoxNTQ0MDUyNTAwLCJleHAiOjE1NDQwNjI1ODB9.cXuNTsczoiUvgq0_OLyfKlVCHYnBmIWbfcBlygZPTu8'
+        console.log(this.token);
+        // this.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzA4NWYxNDMzYjJhMmExMzA5MWYzM2YiLCJmaXJzdE5hbWUiOiJKZW5ueSIsImxhc3ROYW1lIjoiVXJiYW4iLCJlbWFpbCI6Imdhcm9jb2xhMjJAeWFob28uY29tIiwicGhvbmUiOiI5MTczOTk4OTA3IiwiaWF0IjoxNTQ0MDUyNTAwLCJleHAiOjE1NDQwNjI1ODB9.cXuNTsczoiUvgq0_OLyfKlVCHYnBmIWbfcBlygZPTu8'
         const configUrl = "http://localhost:3000/api/auth/authorize";
         return this.http.get(configUrl, new RequestOptions({
             headers: new Headers({
@@ -44,7 +45,34 @@ export class ConfigService {
         var myjson = {"email": curremail, "password": currpassword}
         const configUrl = "http://localhost:3000/api/auth/login/";
         const h: Headers=new Headers();
-        return this.http.post(configUrl,myjson,{headers: h});
+        var data = this.http.post(configUrl,myjson,{headers: h});
+        console.log(data);
+        this.http.post(configUrl,myjson,{headers: h}).subscribe(data=>{
+          let token = data.json().token;
+          console.log(token);
+          let array = new Array(2);
+          let counter = 0;
+          let str = "";
+          for (let i = 0;i<token.length;i++){
+              if (token.charAt(i)==' '){
+                array[counter]=str;
+                counter+=1;
+                str = "";
+              }
+              else if (i==token.length-1){
+                str+=token.charAt(i);
+                array[counter]=str;
+                counter+=1;
+              }
+              else{
+                // console.log(token.charAt(i));
+                str+=token.charAt(i);
+              }
+          }
+          token = array[1];
+          console.log(token);
+        });
+        return data;
     }
     getDogFilter = (ans):Observable<any> => {
         const configUrl = "http://localhost:3000/api/dogs/environment=" + ans[0] + 
